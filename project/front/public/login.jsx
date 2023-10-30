6d17993912904b6cdd5e76ee21e538861c9e9951
DanielBelz1997 <128147522+DanielBelz1997@users.noreply.github.com>
2023-10-30 22:46:36 +0200

    lalala

--- project/front/public/login.jsx
+++ project/front/public/login.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import image from "./5087579.png";
 // import { useNavigate } from "react-router-dom";
 
 const Login = () => {
@@ -14,38 +15,45 @@ const Login = () => {
 
   return (
     <>
-      <div className={"mainContainer"}>
-        <div className={"titleContainer"}>
-          <div>התחבר</div>
+      <div className="container">
+        <div className="imageLogin">
+          <div className="image">
+            <img src={image} alt="Chat" />
+          </div>
         </div>
-        <br />
-        <div className={"inputContainer"}>
-          <input
-            value={username}
-            placeholder="username"
-            onChange={(ev) => setUsername(ev.target.value)}
-            className={"inputBox"}
-          />
-          <label className="errorLabel">{}</label>
-        </div>
-        <br />
-        <div className={"inputContainer"}>
-          <input
-            value={password}
-            placeholder="password"
-            onChange={(ev) => setPassword(ev.target.value)}
-            className={"inputBox"}
-          />
-          <label className="errorLabel">{}</label>
-        </div>
-        <br />
-        <div className={"inputContainer"}>
-          <input
-            className={"inputBotton"}
-            type="button"
-            onClick={onBottonClick}
-            value={"התחבר"}
-          />
+        <div className={"mainContainer"}>
+          <div className={"titleContainer"}>
+            <div>התחבר</div>
+          </div>
+          <br />
+          <div className={"inputContainer"}>
+            <input
+              value={username}
+              placeholder="username"
+              onChange={(ev) => setUsername(ev.target.value)}
+              className={"inputBox"}
+            />
+            <label className="errorLabel">{}</label>
+          </div>
+          <br />
+          <div className={"inputContainer"}>
+            <input
+              value={password}
+              placeholder="password"
+              onChange={(ev) => setPassword(ev.target.value)}
+              className={"inputBox"}
+            />
+            <label className="errorLabel">{}</label>
+          </div>
+          <br />
+          <div className={"inputContainer"}>
+            <input
+              className={"inputBotton"}
+              type="button"
+              onClick={onBottonClick}
+              value={"התחבר"}
+            />
+          </div>
         </div>
       </div>
     </>
