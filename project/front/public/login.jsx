23addbb3f592ea0117f3aa7c0fb6fdafb2712b0f
DanielBelz1997 <128147522+DanielBelz1997@users.noreply.github.com>
2023-10-30 23:55:46 +0200

    front work

--- project/front/public/login.jsx
+++ project/front/public/login.jsx
@@ -5,25 +5,49 @@ import image from "./5087579.png";
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [usernameError, setUsernameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
   // const [submittedData, setSubmittedData] = useState(null);
 
   // const navigate = useNavigate();
 
   const onBottonClick = () => {
-    // what we will do here?
+    setUsernameError("");
+    setPasswordError("");
+
+    if ("" === username) {
+      setUsernameError("Please provide your username");
+      return;
+    }
+
+    if ("" === password) {
+      setPasswordError("Please enter a password");
+      return;
+    }
+
+    if (password.length < 5) {
+      setPasswordError("The password must be 8 characters or longer");
+      return;
+    }
   };
 
   return (
     <>
+      {/* <img src={image} alt="Chat" /> */}
       <div className="container">
         <div className="imageLogin">
-          <div className="image">
-            <img src={image} alt="Chat" />
-          </div>
+          {/* <div className="image"> */}
+          <img src={image} alt="Chat" />
+          {/* </div> */}
         </div>
         <div className={"mainContainer"}>
           <div className={"titleContainer"}>
-            <div>התחבר</div>
+            <div>Login</div>
           </div>
           <br />
           <div className={"inputContainer"}>
@@ -33,17 +57,24 @@ const Login = () => {
               onChange={(ev) => setUsername(ev.target.value)}
               className={"inputBox"}
             />
-            <label className="errorLabel">{}</label>
+            <label className="errorLabel">{usernameError}</label>
           </div>
           <br />
           <div className={"inputContainer"}>
             <input
+              type={showPassword ? "text" : "password"}
               value={password}
               placeholder="password"
               onChange={(ev) => setPassword(ev.target.value)}
               className={"inputBox"}
             />
-            <label className="errorLabel">{}</label>
+            <label className="errorLabel">{passwordError}</label>
+            <span
+              onClick={togglePasswordVisibility}
+              style={{ cursor: "pointer", alignSelf: "center" }}
+            >
+              {showPassword ? "👁️" : "👁️"}
+            </span>
           </div>
           <br />
           <div className={"inputContainer"}>
@@ -51,7 +82,7 @@ const Login = () => {
               className={"inputBotton"}
               type="button"
               onClick={onBottonClick}
-              value={"התחבר"}
+              value={"Login"}
             />
           </div>
         </div>
