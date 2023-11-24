20a3eb8d12fd13ac359d57b1598ff716fd976b0c
DanielBelz1997 <128147522+DanielBelz1997@users.noreply.github.com>
2023-11-24 14:16:09 +0200

    final tsconfig selution

--- project/server/src/controllers/users.ts
+++ project/server/src/controllers/users.ts
@@ -1,19 +1,28 @@
-require("dotenv").config();
 import { Request, Response } from "express";
 import { promisify } from "util";
 import jwt from "jsonwebtoken";
 import { connection } from "../db/connect";
+import { QueryOptions } from "mysql2";
 
-const queryAsync = promisify(connection.query).bind(connection);
+const queryAsync = promisify(connection.query.bind(connection)) as (
+  options: QueryOptions
+) => Promise<any[]>;
+
+async function runQuery(query: string, params: any[]): Promise<any[]> {
+  return queryAsync({
+    sql: query,
+    values: params,
+  });
+}
 
 export async function getHomePage(req: Request, res: Response) {
-  return res.send("this is the home page.");
+  return res.send("This is the home page.");
 }
 
 export async function loginUser(req: Request, res: Response) {
-  const query = `
-  SELECT username, password FROM users
-  WHERE username = ? AND password = ?
+  const query: string = `
+    SELECT username, password FROM users
+    WHERE username = ? AND password = ?
   `;
 
   try {
@@ -23,6 +32,8 @@ export async function loginUser(req: Request, res: Response) {
       return res.status(402).json({ message: "All fields are required" });
     }
 
+    const results = await runQuery(query, [username, password]);
+
     if (results.length === 0) {
       return res.status(401).json({ message: "המשתמש אינו רשום במערכת" });
     }
@@ -31,8 +42,6 @@ export async function loginUser(req: Request, res: Response) {
       username,
       loginTime: Date.now(),
     };
-
-    // ^ DONT PUT ENYTHING THAT SHOULD NOT TO BE PUBLIC! ^
 
     const token = jwt.sign(loginData, process.env.ACCESS_TOKEN_SECRET, {
       expiresIn: "1h",
